d5859c4fb93a3b3ecbdffc31db7885fe3d500293
sauravnayak2224 <sauravnayak03@gmail.com>
2023-03-29 10:30:05 +1100

    Add project files.

---
+++ BookStoreWithDB/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStoreWithDB.Models;
+
+public partial class Book
+{
+    public string BookId { get; set; } = null!;
+
+    public string BookName { get; set; } = null!;
+
+    public int? CustomerId { get; set; }
+}
---
+++ BookStoreWithDB/Models/BookStoreContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWithDB.Models;
+
+public partial class BookStoreContext : DbContext
+{
+    public BookStoreContext()
+    {
+    }
+
+    public BookStoreContext(DbContextOptions<BookStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=SNAYAK\\SQLExpress01;Database=BookStore;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.Property(e => e.BookId)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.BookName)
+                .HasMaxLength(50)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
